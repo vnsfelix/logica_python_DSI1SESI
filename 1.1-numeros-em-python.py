e8bee25ae1a6492709157f3bed1bcbc326650219
Felix <vfelix2022santos@gmail.com>
2026-05-06 11:25:01 -0300

    aula_senai_python

---
+++ 1.1-numeros-em-python.py
@@ -0,0 +1,32 @@
+# AULA COMPLETA: NUMEROS EM PYHTON
+
+"""
+Vamos aprender:
+1 - Tipos numérios
+2- Conversões de tipos
+3 - Hierarquia numérica
+4 - Operações matemáticas
+5 - Coerção de tipos
+6 - Verificação de tipos
+7 - Entrada de dados
+"""
+#=============================
+## PASSO 01 - TIPOS NUMERÍCOS
+#=============================
+# INT -> numeros inteiros
+# float -> numeros com casas decimais
+# complex -> numeros complexos (matematica/engenharia)
+
+print("===== TIPOS NUMERICOS =====")
+
+# EXEMPLO 1 - NUMERO INTEIRO
+
+# criamos uma variavel chamada numero_inteiro
+numero_inteiro = 10
+
+# mostramos o valor
+print("Valor:", numero_inteiro)
+# Type() mostra qual é o tipo da variavel
+print("Tipo:", type(numero_inteiro))
+
+print("-----------------------------")
