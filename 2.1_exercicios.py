b7927b8c20c7cb2c54bd30320c3abdff5ede1c7e
Felix <vfelix2022santos@gmail.com>
2026-05-27 10:42:59 -0300

    meio dos exercicios

--- 2.1_exercicios.py
+++ 2.1_exercicios.py
@@ -67,10 +67,15 @@ print(poema)
 # Use concatenação automática de literais para unir
 # as palavras "Volei" e "top!".
 
+str7 = ("Volei" " Top")
+print(str7)
 
 # EX8
 # Concatene automaticamente as strings
 # "Python", " é ", "demais" em uma única string.
+
+str8 = ("Python" "é" "demais")
+print(str8)
 
 
 # ======================================================
@@ -81,25 +86,38 @@ print(poema)
 # Atribua "software" a uma variável chamada st
 # e mostre a primeira letra da string.
 
+st = "software"
+print("Primeira letra:", st[0])
+
 # EX10
 # Usando a mesma string "software",
 # mostre a última letra.
+
+print("Ultima letra:", st[-1])
 
 # EX11
 # Mostre os caracteres do índice 1 até o índice 4
 # (sem incluir o 4) da string "software ".
 
+print("índice 1 até o índice 4:", st[1:4])
+
 # EX12
 # Mostre os caracteres do início até o índice 3
 # da string "software".
+
+print("inicio até o índice 3:", st[0:3])
 
 # EX13
 # Mostre os caracteres do índice 2 até o final
 # da string "software".
 
+print("índice 2 até o final:", st[2:])
+
 # EX14
 # Mostre o tamanho da string "software"
 # usando a função len().
+
+print ("Tamanho", len(st))
 
 # EX15
 # Acesse o último caractere de "software"
